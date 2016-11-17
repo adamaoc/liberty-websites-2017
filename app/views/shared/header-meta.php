ca4514203347e703f0d8f3f995b37b2b35a04a4a
Adam Moore <adamaoc@gmail.com>
2016-11-17 06:23:05 -0600

    adding header meta, sitemap, and favicons

---
+++ app/views/shared/header-meta.php
@@ -0,0 +1,18 @@
+<link rel="author" href="http://liberty-websites.com" />
+<link rel="publisher" href="http://liberty-websites.com" />
+<meta name="keywords"  content="liberty websites, web hosting, wordpress hosting, wordpress, political blog, politics, political website" />
+<link rel="canonical" href="http://liberty-websites.com" />
+
+<meta property="og:title" content="Liberty Websites - Make a difference BE POLITICALLY ACTIVE" />
+<meta property="og:url" content="http://liberty-websites.com/" />
+<meta property="og:image" content="http://liberty-websites.com/public/images/flag-bg-2.png" />
+<meta property="og:site_name" content="Liberty Websites" />
+<meta property="og:description" content="At Liberty Websites we want to help you get your voice heard. If 2016 has proven anything, it's that traditional media is dying off and you can really make a difference by putting your voice or platform online. We've helped get a few grassroots campaigns and blogs started up throughout this past election cycle and we're eager to help more people. Whether you're wanting to get your word out with a simple blog or start a movement with a political website, we can help." />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@adamaoc" />
+<meta name="twitter:domain" content="liberty-websites.com" />
+<meta name="twitter:title" content="Liberty Websites - Make a difference BE POLITICALLY ACTIVE" />
+<meta name="twitter:description" content="At Liberty Websites we want to help you get your voice heard. If 2016 has proven anything, it's that traditional media is dying off and you can really make a difference by putting..." />
+<meta name="twitter:image" content="http://liberty-websites.com/public/images/flag-bg-2.png" />
+<meta itemprop="image" content="http://liberty-websites.com/public/images/flag-bg-2.png" />
--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -3,14 +3,24 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <meta name="description" content="">
+    <title>Liberty Websites - Make a difference BE POLITICALLY ACTIVE</title>
+    <meta name="description" content="At Liberty Websites we want to help you get your voice heard. If 2016 has proven anything, it's that traditional media is dying off and you can really make a difference by putting your voice or platform online. We've helped get a few grassroots campaigns and blogs started up throughout this past election cycle and we're eager to help more people. Whether you're wanting to get your word out with a simple blog or start a movement with a political website, we can help.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <!-- Place favicon.ico in the root directory -->
+    <meta name="google-site-verification" content="tlo4p-JW6tGcM7dTKAg1vPRD-JLtmv6nL6vkF4j9yCU" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicons/manifest.json">
+    <link rel="mask-icon" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="liberty-websites">
+    <meta name="application-name" content="liberty-websites">
+    <meta name="theme-color" content="#ffffff">
 
     <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/public/css/main.css">
+
+    <?php getShared('header-meta'); ?>
   </head>
   <body>
     <?php getComponent('navigation'); ?>
